df61c4b8febcf93691298fd17f4cd7a2ba86e1e5
Shawn de Jesus <43559075+smsdejesus@users.noreply.github.com>
2026-07-02 23:32:28 -0700

    Add: python-for-loops-step - submission-0

---
+++ Python For Beginners/python-for-loops-step/submission-0.py
@@ -0,0 +1,7 @@
+for i in range(0, 110, 10):
+    print(i)
+
+# Note: We could have passed in any number from 
+# [101 - 110] as the second argument to range.
+# Once i reaches or exceeds the second argument,
+# the loop will stop.
